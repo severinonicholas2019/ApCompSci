3ea2c74ac35d10cb2a52e1bfa959fe192d01d1a9
Nicholas Severino <42677914+severinonicholas2019@users.noreply.github.com>
2019-03-22 08:31:12 -0700

    mage portion

--- BattleRoyale/Mage.java
+++ BattleRoyale/Mage.java
@@ -1,1 +1,20 @@
-
+import java.util.Random;
+public class Mage extends Player{
+    String name;
+    private int wisdom;
+    public Mage(String newName) {
+        name = newName;
+        Random rand = new Random();
+        strength = 7 + (rand.nextInt(6) + 1);
+        intelligence = 15 + (rand.nextInt(6) + 1);
+        agility = 8 + (rand.nextInt(6) + 1);
+        wisdom = 10 + (rand.nextInt(6) + 1);
+        hp = hpmax = (strength);
+        mp = mpmax = (intelligence + 2 * wisdom);
+    }
+    public void stats() {
+        System.out.printf("%s, a Mage:\n", name);
+        System.out.printf("WIS: %3d \n", wisdom);
+        super.stats();
+    }
+}
