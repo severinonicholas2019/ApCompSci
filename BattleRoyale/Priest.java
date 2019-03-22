9dccad2f7219d6b8d34777eb822b63f7d7c157b3
Nicholas Severino <42677914+severinonicholas2019@users.noreply.github.com>
2019-03-22 08:33:14 -0700

    Update Priest.java

--- BattleRoyale/Priest.java
+++ BattleRoyale/Priest.java
@@ -1,1 +1,22 @@
+import java.util.Random;
+public class Priest extends Player{
+    String name;
+    private int charisma;
+    public Priest(String newName) {
+        name = newName;
+        Random rand = new Random();
+        strength = 7 + (rand.nextInt(6) + 1);
+        intelligence = 15 + (rand.nextInt(6) + 1);
+        agility = 8 + (rand.nextInt(6) + 1);
+        charisma = 10 + (rand.nextInt(6) + 1);
+        hp = hpmax = (strength);
+        mp = mpmax = (intelligence + 2 * charisma);
+    }
+    public void stats() {
+        System.out.printf("%s, a Priest:\n", name);
+        System.out.printf("CHA: %3d \n", charisma);
+        super.stats();
+    }
+}
+
 
